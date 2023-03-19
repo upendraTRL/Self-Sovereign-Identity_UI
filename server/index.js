9a39c393c75e0787f8dd9ec61e1211e97e17bba0
upendraTRL <upendrataral21@gmail.com>
2023-03-19 15:24:03 +0530

    Login and Registration, Done.

--- server/index.js
+++ server/index.js
@@ -1,40 +1,42 @@
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
+// Issues: -
+// 1. Login checks only first occurance of username. Eg: two records - abc:123, abc:abc
+//    If entered abc, it'll check first abc.
 
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const session = require("express-session");
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
 
-// const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(
-//     cors({
-//         origin: ["http://localhost:3000"],
-//         methods: ["GET", "POST"],
-//         credentials: true,
-//     })
-// );
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(
-//     session({
-//         key: "userId",
-//         secret: "subscribe",
-//         resave: false,
-//         saveUninitialized: false,
-//         cookie: {
-//             expires: 60 * 60 * 24,
-//         },
-//     })
-// );
+app.use(
+    session({
+        key: "userId",
+        secret: "subscribe",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 60 * 24,
+        },
+    })
+);
 
 const db = mysql.createConnection({
     user: 'root',
@@ -46,64 +48,75 @@ const db = mysql.createConnection({
 app.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    db.query(
-        "INSERT INTO users (username, password) VALUES (?,?)",
-        [username, password],
-        (err, result) => {
-            // console.log(err);
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
             console.log(err);
+        }
+
+        db.query(
+            "INSERT INTO users (username, password) VALUES (?,?)",
+            [username, hash],
+            (err, result) => {
+                console.log(err);
+            }
+        );
+    });
+});
+
+app.get("/login", (req, res) => {
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user });
+    } else {
+        res.send({ loggedIn: false });
+    }
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE username = ?;",
+        username,
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    // console.log(password, result[0].password, response);
+                    if (response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
+                        res.send(result);
+                    } else {
+                        res.send({ message: "Wrong username/password combination!" });
+                    }
+                });
+            } else {
+                res.send({ message: "User doesn't exist" });
+            }
         }
     );
 });
 
-
-// app.get("/login", (req, res) => {
-//     if (req.session.user) {
-//         res.send({ loggedIn: true, user: req.session.user });
-//     } else {
-//         res.send({ loggedIn: false });
-//     }
-// });
-
-// app.post("/login", (req, res) => {
-//     const username = req.body.username;
-//     const password = req.body.password;
-
-//     db.query(
-//         "SELECT * FROM users WHERE username = ?;",
-//         username,
-//         (err, result) => {
-//             if (err) {
-//                 res.send({ err: err });
-//             }
-
-//             if (result.length > 0) {
-//                 bcrypt.compare(password, result[0].password, (error, response) => {
-//                     if (response) {
-//                         req.session.user = result;
-//                         console.log(req.session.user);
-//                         res.send(result);
-//                     } else {
-//                         res.send({ message: "Wrong username/password combination!" });
-//                     }
-//                 });
-//             } else {
-//                 res.send({ message: "User doesn't exist" });
-//             }
-//         }
-//     );
-// });
-
-
-// app.get('/', (req, res) => {
-
-//     const sqlInsert = "INSERT INTO users (username, password) VALUES (test,test)";
-
-//     db.query(sqlInsert, (err, result) => {
-//         res.send("Inserted");
-//     });
-// });
-
 app.listen(3001, () => {
-    console.log("Server Running! on port 3001");
+    console.log("running server");
 });
+
+
+
+
+
+
+// My Part
+// "INSERT INTO users (username, password) VALUES (?,?)",
+
+// const db = mysql.createConnection({
+//     user: 'root',
+//     host: 'localhost',
+//     password: 'root',
+//     database: 'loginsystem'
+// });
