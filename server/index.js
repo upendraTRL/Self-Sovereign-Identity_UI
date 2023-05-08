1c5b21f8f9ef380d0a37da6d7bc5bdfcb827246b
Gaurav <gaurvabade3181@gmail.com>
2023-05-08 19:41:40 +0530

    Call remote api from node

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-// css
+// Issues: -
+// 1. Login checks only first occurance of username. Eg: two records - abc:123, abc:abc
+//    If entered abc, it'll check first abc.
 
 const express = require("express");
-const mysql = require("mysql2"); //imp to use mysql2 instead of mysql1, to avoid following error.
-//Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; 
-//consider upgrading MySQL client
+const mysql = require("mysql");
 const cors = require("cors");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -38,18 +39,16 @@ app.use(
     })
 );
 
-const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'root',
-    database: 'loginsystem'
-});
+// const db = mysql.createConnection({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     password: process.env.DB_PASS,
+//     database: process.env.DATABASE
+// });
 
 app.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const usertype = req.body.usertype;
-    const displayname = req.body.displayname;
 
     bcrypt.hash(password, saltRounds, (err, hash) => {
         if (err) {
@@ -57,14 +56,9 @@ app.post("/register", (req, res) => {
         }
 
         db.query(
-            "INSERT INTO users (username, password, usertype, displayname) VALUES (?,?,?,?)",
-            [username, hash, usertype, displayname],
+            "INSERT INTO users (username, password) VALUES (?,?)",
+            [username, hash],
             (err, result) => {
-                console.log(result);
-                if (typeof err === "object") {
-                    console.log('Successful registration!');
-                    res.status(200).send("Success");
-                }
                 console.log(err);
             }
         );
@@ -93,13 +87,11 @@ app.post("/login", (req, res) => {
 
             if (result.length > 0) {
                 bcrypt.compare(password, result[0].password, (error, response) => {
-                    console.log(password, result[0].password, response);
+                    // console.log(password, result[0].password, response);
                     if (response) {
-
                         req.session.user = result;
                         console.log(req.session.user);
                         res.send(result);
-                        // console.log("RESULT - " + result);
                     } else {
                         res.send({ message: "Wrong username/password combination!" });
                     }
@@ -111,8 +103,65 @@ app.post("/login", (req, res) => {
     );
 });
 
-app.listen(3001, () => {
-    console.log("running server");
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////CALL API FROM NESTJS//////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///  axios for remote API call
+const axios = require('axios');
+
+
+//create connection invitation
+app.post("/connections/create",(req,res)=>{
+
+      console.log("Inside create connections")
+    axios.post(`http://${process.env.NEST_IP}:${process.env.NEST_PORT}/connections/create-invitation`)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+})
+
+//receive connection invitation
+app.post("/connections/receive",(req,res)=>{
+
+    console.log("Inside receive connections")
+  axios.post(`http://${process.env.NEST_IP}:${process.env.NEST_PORT}/connections/receive-invitation`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+})
+
+
+//send issue credential
+app.post("/issue-credential/send",(req,res)=>{
+
+    console.log("Inside issue credential send")
+  axios.post(`http://${process.env.NEST_IP}:${process.env.NEST_PORT}/issue-credential/send-credential`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+})
+
+
+
+
+
+
+
+app.listen(process.env.SERVER, () => {
+    console.log("running on server",process.env.SERVER);
 });
 
 
